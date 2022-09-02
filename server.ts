8d03dbba92e2942b187563531929496c469fa718
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-02 15:37:54 +0700

    confix : os

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import http, {Server, IncomingMessage, ServerResponse} from 'http';
+import os from 'os';
 
 const hostname :string = '127.0.0.1';
 const port:number = 5000;
@@ -6,7 +7,14 @@ const port:number = 5000;
 const server:Server = http.createServer((request:IncomingMessage, response:ServerResponse)=>{
     response.statusCode = 200;
     response.setHeader ('Content-Type','text/html');
-    response.end(`<h3 style="color:green"> welcome to node js server </h3>`)
+    let osData = {
+        totalMemory :os.totalmem(), 
+        freeMemory: os.freemem(),
+        homedir: os.homedir(),
+        computerName: os.hostname()
+    }
+
+    response.end(`<pre style="color:green">${JSON.stringify(osData)}</pre>`)
 })
 
 
